3246f8bc8c3811a5bff116bf05e068aed9a2c792
fuzhiqiang1230 <fuzhiqiang1230@outlook.com>
2017-12-21 11:27:10 +0800

    一些改变

--- FML.py
+++ FML.py
@@ -8,18 +8,34 @@ def loadmat(path):
     return data
 
 
-def findknn(data, k=2):
-    row, col = data.shape
+def findknn(datain, k=2):
+    row, col = datain.shape
+    data = datain.astype(np.float64)
     distlist = np.zeros([row, row])
     numlist = np.zeros([row, k])
     for i in range(row):
         for j in range(row):
             distlist[i, j] = np.linalg.norm(data[i, :]-data[j, :])
         listnew = []
-        listnew = distlist[i, :]
-        listnew.sort()
+        listnew = np.sort(distlist[i, :])
         listnew = listnew[1:k+1]
         for l in range(k):
             local = np.where(distlist[i, :] == listnew[l])
             numlist[i, l] = local[0]
     return numlist
+
+
+def constructW(datain):
+    data = datain.astype(np.int32)
+    row, col = data.shape
+    w = np.zeros([row, row])
+    for i in range(row):
+        w[i, data[i, :]] = 1
+    return w
+
+
+def computeD(datain):
+    result = datain.sum(axis=1) 
+    result = np.diag(result)
+    return result
+
--- test.py
+++ test.py
@@ -6,11 +6,12 @@ Created on Fri Dec  1 21:37:14 2017
 """
 import numpy as np
 from FML import loadmat
-from FML import findknn
+from FML import findknn,constructW,computeD
 data = loadmat('Yale_32x32.mat')
 a = data['fea']
 row,col = a.shape
 distlist = np.zeros([row,row])
 b =[]
-b=a[1,:]
-#b = findknn(a)
+b = findknn(a)
+w = constructW(b)
+D = computeD(w)
